2f7df4a585083be74b4f999107915a8d5f7a79a9
Robert Serrano Kobylyansky <robertkobylyansky@gmail.com>
2024-09-30 21:11:40 +0100

    button led

--- src/bin/hal_demo.rs
+++ src/bin/hal_demo.rs
@@ -6,7 +6,7 @@ use defmt_rtt as _; // global logger
 use panic_probe as _; // panic handler
 use stm32wlxx_hal::{
     cortex_m,
-    gpio::{pins, Input, PinState, PortC, Pull},
+    gpio::{pins, Input, Output, OutputArgs, PinState, PortB, PortC, Pull},
     pac,
 };
 
@@ -15,14 +15,24 @@ fn main() -> ! {
     let mut dp: pac::Peripherals = defmt::unwrap!(pac::Peripherals::take());
 
     let gpioc: PortC = PortC::split(dp.GPIOC, &mut dp.RCC);
-    let button_1: Input<pins::C6> =
+    let button: Input<pins::C6> =
         cortex_m::interrupt::free(|cs| Input::new(gpioc.c6, Pull::Up, cs));
 
-    let mut prev_level: PinState = button_1.level();
+    let gpiob: PortB = PortB::split(dp.GPIOB, &mut dp.RCC);
+    let mut led: Output<pins::B9> = cortex_m::interrupt::free(|cs| {
+        Output::new(
+            gpiob.b9,
+            &OutputArgs::default(),
+            cs,
+        )
+    });
+
+    let mut prev_level: PinState = button.level();
     loop {
-        let level: PinState = button_1.level();
+        let level: PinState = button.level();
         if level != prev_level {
             prev_level = level;
+            led.set_level(!level);
             defmt::info!(
                 "Button {}",
                 match level {
